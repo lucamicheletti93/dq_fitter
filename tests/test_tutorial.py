03891ccdafd383fcc046d8c622b877e6eba2bba3
Lucamicheletti93 <luca.mike93@gmail.com>
2025-08-28 18:01:13 +0200

    Adding a test for the fitter

--- tests/test_tutorial.py
+++ tests/test_tutorial.py
@@ -12,7 +12,67 @@ from ROOT import gRandom
 sys.path.append('../')
 from DQFitter import DQFitter
 
+def GenerateTutorialSample():
+    print("----------- GENERATE TUTORIAL SAMPLE -----------")
+    nEvents = 100000
+    SigOverBkg1 = 0.03
+    SigOverBkg2 = SigOverBkg1 / 10.
+    
+    fOut = TFile("tutorial.root", "RECREATE")
+
+    funcMassBkg = TF1("funcMassBkg", "expo", 0., 5.)
+    funcMassBkg.SetParameter(0, 0.00)
+    funcMassBkg.SetParameter(1, -0.5)
+
+    funcMassSig1 = TF1("funcMassSig1", "gaus", 0., 5.)
+    funcMassSig1.SetParameter(0, 1.0)
+    funcMassSig1.SetParameter(1, 3.096)
+    funcMassSig1.SetParameter(2, 0.070)
+
+    funcMassSig2 = TF1("funcMassSig2", "gaus", 0., 5.)
+    funcMassSig2.SetParameter(0, 1.0)
+    funcMassSig2.SetParameter(1, 3.686)
+    funcMassSig2.SetParameter(2, 1.05 * 0.070)
+
+    histMass = TH1F("histMass", "histMass", 100, 0., 5.)
+    histMass.FillRandom("funcMassBkg", int(nEvents - (nEvents * SigOverBkg1)))
+    histMass.FillRandom("funcMassSig1", int(nEvents * SigOverBkg1))
+    histMass.FillRandom("funcMassSig2", int(nEvents * SigOverBkg2))
+    histMass.Write()
+
+    print("Data histogram")
+    print("counter J/psi = %f" % (int(nEvents * SigOverBkg1)))
+    print("counter Psi(2S) = %f" % (int(nEvents * SigOverBkg2)))
+
+    counterSig1 = 0
+    counterSig2 = 0
+
+    fMass = array('f', [0.])
+    tree = TTree("data", "data")
+    tree.Branch("fMass", fMass, "fMass/F")
+
+    for iEvent in range(0, nEvents):
+        seed = gRandom.Rndm()
+        if seed > SigOverBkg1:
+            fMass[0] = funcMassBkg.GetRandom()
+        else:
+            if seed > SigOverBkg2:
+                fMass[0] = funcMassSig1.GetRandom()
+                counterSig1 = counterSig1 + 1
+            else:
+                fMass[0] = funcMassSig2.GetRandom()
+                counterSig2 = counterSig2 + 1
+        tree.Fill()
+    tree.Write()
+
+    fOut.Close()
+
+    print("Data tree")
+    print("counter J/psi = %f" % (counterSig1))
+    print("counter Psi(2S) = %f" % (counterSig2))
+
 def test_tutorial():
+    GenerateTutorialSample()
     here = pathlib.Path(__file__).parent
     cfg_path = here / ".." / "tutorial" / "config_tutorial_fit.json"
     with open(cfg_path, 'r') as jsonCfgFile:
